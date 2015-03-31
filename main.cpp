1a92eff886b0b55bf8cf7c51f7922b2abe0fb1fb
Parker Esmay <pdesmay@gmail.com>
2015-03-31 01:13:39 -0700

    Add files
    Added all files from assignment 4.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,28 @@
-
 /*
-    Retrieved from: http://www.lifengadget.com/lifengadget/program-to-check-whether-a-number-is-prime-or-not-in-cplusplus/
+ 	Retrieved from: http://www.lifengadget.com/lifengadget/program-to-check-whether-a-number-is-prime-or-not-in-cplusplus/
     primenumber.cpp
     Program to check whether the number is prime or not in C++
     To compile (Need g++ compiler for unix/linux or Microsoft compiler for windows)
     g++ primenumber.cpp -o  prime
     To exectue.
     ./prime
-
+ 
     For windows
     c:\directory> cl -GX primenumber.cpp
 */
 #include <iostream>
-//#include <config.h>
-
+ 
 using namespace std;
-
+ 
 int main()
  {
-
-     int num = 0;
-	 int count = 0;
-	 
-     while (num != -1)
-     {
-     
-     cout<<"\nEnter -1 to exit the program\n";
-	 cout<<"Enter the number to check : ";
+ 
+     int num,count=0;
+     cout<<"Enter the number to check : ";
      cin>>num;
-
-     for(int i=2;i<num;i++)
-
+ 
+     for(int i=2;i<num;i++)        
+ 
          {
             if(num%i==0)       // checks if the number is fully divisible or not
              {
@@ -39,12 +31,11 @@ int main()
          }
             if(count==0)
              {
-               cout<<"\nIt is a Prime Number \n";
+               cout<<"It is a Prime Number \n";
              }
             else
              {
-              cout<<"\nIt is not a Prime Number \n";
+              cout<<"It is not a Prime Number \n";
              }
-	}
-
+ 
  }
