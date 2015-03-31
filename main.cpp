f2f8e28f99c69f1accd847f14968bec95069c5bf
Parker Esmay <pdesmay@gmail.com>
2015-03-31 02:46:14 -0700

    remove semicolon

--- main.cpp
+++ main.cpp
@@ -6,23 +6,23 @@
     g++ primenumber.cpp -o  prime
     To exectue.
     ./prime
- 
+
     For windows
     c:\directory> cl -GX primenumber.cpp
 */
 #include <iostream>
- 
+
 using namespace std;
- 
+
 int main()
  {
- 
+
      int num,count=0;
-     cout<<"Enter the number to check : ";
+     cout<<"Enter the number to check : "
      cin>>num;
- 
-     for(int i=2;i<num;i++)        
- 
+
+     for(int i=2;i<num;i++)
+
          {
             if(num%i==0)       // checks if the number is fully divisible or not
              {
@@ -37,5 +37,5 @@ int main()
              {
               cout<<"It is not a Prime Number \n";
              }
- 
+
  }
